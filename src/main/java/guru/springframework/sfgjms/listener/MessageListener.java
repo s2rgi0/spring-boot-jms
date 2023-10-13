658497c3c8875e4454892a79e7f1d3d7c3db5b35
s2rgi0 <tlakolu@gmail.com>
2023-10-13 11:51:38 -0600

    added connection to message broker

--- src/main/java/guru/springframework/sfgjms/listener/MessageListener.java
+++ src/main/java/guru/springframework/sfgjms/listener/MessageListener.java
@@ -37,7 +37,8 @@ public class MessageListener {
 
     @JmsListener(destination = JmsConfig.MY_SEND_RCV_QUEUE)
     public void listenForHello(@Payload HelloWorldMessage helloWorldMessage,
-                               @Headers MessageHeaders headers, Message message) throws JMSException {
+                               @Headers MessageHeaders headers, Message jmsMessage,
+                               org.springframework.messaging.Message springMessage) throws JMSException {
 
         HelloWorldMessage payloadMsg = HelloWorldMessage
                 .builder()
@@ -45,8 +46,9 @@ public class MessageListener {
                 .message("World!!")
                 .build();
 
-        jmsTemplate.convertAndSend(message.getJMSReplyTo(), payloadMsg);
+        //example to use Spring Message type
+        // jmsTemplate.convertAndSend((Destination) springMessage.getHeaders().get("jms_replyTo"), "got it!");
 
+        jmsTemplate.convertAndSend(jmsMessage.getJMSReplyTo(), payloadMsg);
     }
-
 }
